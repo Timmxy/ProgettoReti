685a08a5949e658eaef7ded4ce2e6182913f70eb
Jacopo De Rosa <jacopo.derosa00@gmail.com>
2025-02-06 15:10:31 +0100

    json salva più avatar

--- Assets/Scripts/SaveAvatar.cs
+++ Assets/Scripts/SaveAvatar.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using TMPro;
@@ -9,10 +10,11 @@ public class SaveAvatar : MonoBehaviour
     public void GenerateStringId(GameObject avatar)
     {
         string id = avatar.GetComponent<Avatar>().GetGenderId() +
-                   avatar.GetComponent<Avatar>().GetHeadId() +
-                   avatar.GetComponent<Avatar>().GetBodyId() +
-                   avatar.GetComponent<Avatar>().GetLegsId() +
-                   avatar.GetComponent<Avatar>().GetSkinId();
+                    avatar.GetComponent<Avatar>().GetHeadId() +
+                    avatar.GetComponent<Avatar>().GetBodyId() +
+                    avatar.GetComponent<Avatar>().GetLegsId() +
+                    avatar.GetComponent<Avatar>().GetSkinId();
+        
         // aggiungere System.GUID all'ID
         string guid = Guid.NewGuid().ToString();
         
@@ -27,6 +29,47 @@ public class SaveAvatar : MonoBehaviour
         Debug.Log("ID SALVATO: " + id);
     }
     
+    
+    // Metodo per aggiungere un nuovo avatar al JSON
+    public static void SaveJson(string setId, string setGuid, string folderPath)
+    {
+        string filePath = "C:/Users/j.derosa/Documents/TEST/data.json";
+
+        // Lista degli avatar esistenti
+        AvatarList avatarList = new AvatarList();
+        
+        // Se il file esiste, leggilo e deserializzalo
+        if (File.Exists(filePath))
+        {
+            string existingJson = File.ReadAllText(filePath);
+            if (!string.IsNullOrWhiteSpace(existingJson)) // Evita di deserializzare un file vuoto
+            {
+                avatarList = JsonUtility.FromJson<AvatarList>(existingJson) ?? new AvatarList();
+            }
+        }
+
+        // Aggiungi il nuovo avatar alla lista
+        avatarList.avatars.Add(new DataModel { id = setId, guid = setGuid });
+        Debug.Log( "avatarList: "+ avatarList.ToString());
+
+
+        // Serializza l'intera lista in JSON
+        string jsonString = JsonUtility.ToJson(avatarList, true);
+        Debug.Log("pezzo demmerda01: "+jsonString);
+        
+        // Verifica se la cartella esiste, altrimenti la crea
+        if (!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+        }
+        
+        // Scrivi il JSON nel file
+        File.WriteAllText(filePath, jsonString);
+        
+        Debug.Log($"Nuovo avatar salvato in: {filePath}");
+    }
+    
+    /*
     // Metodo per serializzare e salvare il JSON in un percorso specifico
     private static void SaveJson(string setId, string setGuid, string folderPath)
     {
@@ -54,12 +97,20 @@ public class SaveAvatar : MonoBehaviour
 
         Debug.Log($"Dati JSON salvati con successo in: {filePath}");
     }
+    */
 } 
 
+[System.Serializable]
 public class DataModel
 {
     public string id;
     public string guid;
+}
+
+[System.Serializable]
+public class AvatarList
+{
+    public List<DataModel> avatars = new List<DataModel>();
 }
 
 
