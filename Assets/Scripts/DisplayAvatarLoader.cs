ebf99c183c358242d4a743d7eb256b89625ffe68
Jacopo De Rosa <jacopo.derosa00@gmail.com>
2025-02-07 13:05:44 +0100

    panel load avatar, img caricano avatar - fixare: quando carico, torno ad avatarCreator

---
+++ Assets/Scripts/DisplayAvatarLoader.cs
@@ -0,0 +1,70 @@
+using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DisplayAvatarLoader : MonoBehaviour
+{
+    [SerializeField] private GameObject _button;
+    [SerializeField] private Transform _contentTransform;
+
+    private const string FilePath = "C:/Users/j.derosa/Documents/TEST/data.json"; // path del json
+    
+    public void LoadJson()
+    {
+        // verifica se il file esiste
+        if (File.Exists(FilePath))
+        {
+            try
+            {
+                // legge il contenuto del file JSON
+                string jsonString = File.ReadAllText(FilePath);
+
+                // deserializza il JSON in un array di oggetti DataModel
+                AvatarList data = JsonUtility.FromJson<AvatarList>(jsonString);
+
+                // DEBUG
+                Debug.Log("Dati JSON caricati con successo!");
+
+                // estrae id dal json deserializzato
+                // TODO: aggiungere anche GUID
+                foreach (DataModel avatarInfo in data.avatars)
+                {
+                    // per ciascun avatar, istanzio bottone nel pannello Load 
+                    
+                    // istanzia il prefab del bottone come figlio del content (scrollview)
+                    GameObject temp = Instantiate(_button, _contentTransform);
+                    //  carico img
+                    temp.GetComponentInChildren<UnityEngine.UI.Image>().sprite = Base64ToSprite(avatarInfo.image);
+                    // associo id
+                    temp.GetComponent<LoadAvatar>().SetAvatarId(avatarInfo.id);
+                    
+                    // Opzionale: Reset di posizione, scala e rotazione
+                    _button.transform.localPosition = Vector3.zero;
+                    _button.transform.localScale = Vector3.one;
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogError($"Errore durante la lettura del JSON: {ex.Message}");
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"File JSON non trovato nel percorso: {FilePath}");
+        }
+    }
+    
+    private Sprite Base64ToSprite(string base64)
+    {
+        byte[] imageBytes = Convert.FromBase64String(base64);
+        Texture2D texture = new Texture2D(2, 2);
+        return texture.LoadImage(imageBytes) ? TextureToSprite(texture) : null;
+    }
+
+    private Sprite TextureToSprite(Texture2D texture)
+    {
+        return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+    }
+}
+
--- Assets/Scripts/LoadAvatar.cs
+++ Assets/Scripts/LoadAvatar.cs
@@ -7,76 +7,27 @@ using UnityEditor.Rendering;
 using UnityEngine.Rendering.Universal.Internal;
 
 public class LoadAvatar : MonoBehaviour
-{
-    [SerializeField] private TMPro.TMP_InputField _input;   // textfield da dove prendo id da caricare
-    [SerializeField] private Avatar _avatar;    // riferimento all'avatar in scena
-    [SerializeField] private GameObject _testSprite;
-    
-    private const int NumCategories = 5;    // numero di categorie customizzabili
-    
-    
-    public void LoadJson()
+{ 
+    private Avatar _avatar;    // riferimento all'avatar in scena (script)
+    private string _avatarId;   // codice avatar
+
+    private void OnEnable()
     {
-        string filePath = "C:/Users/j.derosa/Documents/TEST/data.json"; // path del json
-        string id = null;   // id asset dell'avatar
-        
-        
-        // verifica se il file esiste
-        if (File.Exists(filePath))
-        {
-            try
-            {
-                // legge il contenuto del file JSON
-                string jsonString = File.ReadAllText(filePath);
-
-                // deserializza il JSON in un array di oggetti DataModel
-                AvatarList data = JsonUtility.FromJson<AvatarList>(jsonString);
-
-                // DEBUG
-                Debug.Log("Dati JSON caricati con successo!");
-                
-
-                // estrae id dal json deserializzato
-                // TODO: aggiungere anche GUID
-                foreach (DataModel avatarInfo in data.avatars)
-                {
-                    // se è l'id che cerco
-                    if (avatarInfo.id.Equals(_input.text))
-                    {
-                       // carico gli asset corrispondenti
-                       id = avatarInfo.id;
-                       _testSprite.GetComponent<Image>().sprite = Base64ToSprite(avatarInfo.image);
-                    }
-                }
-
-                // controllo lunghezza stringa id
-                /*if (id.Length != 10)
-                {
-                    Debug.LogError("ID non valido. Deve contenere esattamente 10 cifre.");
-                    return;
-                }*/
-
-                // suddivide id in parti uguali (4 per ogni categoria) e applico cambiamenti per ciascuna categoria
-                ApplyChangesToAvatar(id.Substring(0, 4));
-                ApplyChangesToAvatar(id.Substring(4, 4));
-                ApplyChangesToAvatar(id.Substring(8, 4));
-                ApplyChangesToAvatar(id.Substring(12, 4));
-                ApplyChangesToAvatar(id.Substring(16, 4));
-                
-                Debug.Log("Avatar caricato correttamente!");
-            }
-            catch (System.Exception ex)
-            {
-                Debug.LogError($"Errore durante la lettura del JSON: {ex.Message}");
-            }
-        }
-        else
-        {
-            Debug.LogWarning($"File JSON non trovato nel percorso: {filePath}");
-        }
-
+        _avatar = GameObject.FindGameObjectWithTag("Avatar").GetComponent<Avatar>();
     }
-    
+
+    public void LoadAvatarSettings()
+    {
+        // suddivide id in parti uguali (4 per ogni categoria) e applico cambiamenti per ciascuna categoria
+        ApplyChangesToAvatar(_avatarId.Substring(0, 4));
+        ApplyChangesToAvatar(_avatarId.Substring(4, 4));
+        ApplyChangesToAvatar(_avatarId.Substring(8, 4));
+        ApplyChangesToAvatar(_avatarId.Substring(12, 4));
+        ApplyChangesToAvatar(_avatarId.Substring(16, 4));
+
+        Debug.Log("Avatar caricato correttamente!");
+    }
+
     // applica gli asset letti nell'id all'avatar
     private void ApplyChangesToAvatar(string code)
     {
@@ -123,20 +74,9 @@ public class LoadAvatar : MonoBehaviour
         Debug.Log("Se sono qui vuol dire che qualcosa e' andato storto.");
         return null;
     }
-    
-    public Sprite Base64ToSprite(string base64)
-    {
-        byte[] imageBytes = Convert.FromBase64String(base64);
-        Texture2D texture = new Texture2D(2, 2);
-        if (texture.LoadImage(imageBytes))
-        {
-            return TextureToSprite(texture);
-        }
-        return null;
-    }
 
-    private Sprite TextureToSprite(Texture2D texture)
+    public void SetAvatarId(string id)
     {
-        return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+        this._avatarId = id;
     }
 }
